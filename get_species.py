79ab21e12bb77360f6eababce6411a5aeaf2c022
dmartimarti <dmartimarti@gmail.com>
2020-11-20 17:10:35 +0000

    first commit with hits and python script

---
+++ get_species.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 20 16:23:10 2020
+
+@author: dani
+"""
+
+import pandas as pd
+import os
+
+path = '/home/dani/Documents/MRC_postdoc/Cancer/micit_prediction'
+# change dir to my path
+os.chdir(path)
+
+
+hits = pd.read_excel('./sup_info/aay9189_TableS4.xlsx',
+                     sheet_name='Hits',skiprows=2)
+
+cols = ['Breast (N)','Breast (NAT)','Breast (T)','Lung (NAT)','Lung (T)',
+        'Melanoma (T)','Pancreas (T)','Ovary (N+NAT)','Ovary (T)','Bone (T)',
+        'GBM (T)','Colon (NAT)','Colon (T)']
+
+# sum rows
+hits['row_sum'] = hits[cols].sum(axis=1)
+
+is_hit =  hits['row_sum'] >= 1
+
+is_hit
+# store only rows as hits
+hits_redux = hits[is_hit]
+
+
+# remove NaN
+hits_redux = hits_redux[hits_redux['species'].notnull()]
+
+# which have "species" in their species name
+hits_species = hits_redux.species.str.contains('Unknown')
+
+# opposite of previous selection, slopy way I know, but...
+hits_species = hits_species == False
+
+hits_final = hits_redux[hits_species]
+
+hits_final.to_csv('final_hits.csv', index = False)
